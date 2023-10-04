c5ad441e86e070a1c17f4ad6ef68240059a032d1
jithsg <135303624+jithsg@users.noreply.github.com>
2023-10-04 08:09:20 +0000

    added dockerfile, makefile, main.py

--- main.py
+++ main.py
@@ -13,3 +13,42 @@ model = models.densenet121(pretrained=True)
 model.eval()
 imagenet_class_index = json.load(open("imagenet_class_index.json", encoding="utf-8"))
 
+
+def transform_image(image_bytes):
+    try:
+        my_transforms = transforms.Compose(
+            [
+                transforms.Resize(255),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            ]
+        )
+        image = Image.open(io.BytesIO(image_bytes))
+        return my_transforms(image).unsqueeze(0)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Invalid image")
+
+
+def get_prediction(image_bytes):
+    tensor = transform_image(image_bytes=image_bytes)
+    outputs = model.forward(tensor)
+    _, y_hat = outputs.max(1)
+    predicted_idx = str(y_hat.item())
+    return imagenet_class_index[predicted_idx]
+
+
+@app.get("/")
+def index():
+    return {"message": "Hello World"}
+
+
+@app.post("/predict")
+async def predict(file: UploadFile):
+    image_bytes = await file.read()
+    class_id, class_name = get_prediction(image_bytes=image_bytes)
+    return {"class_id": class_id, "class_name": class_name}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
